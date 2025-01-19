074c83ceb44e16688d2b6568bf04a6f874845c5b
Termi <olafkrawczyk13@icloud.com>
2025-01-19 21:53:08 +0100

    Otp

--- Next/opbio/app/otp/page.tsx
+++ Next/opbio/app/otp/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 
 import "./main.css"
-import {getSession} from "@/app/_actions/actions";
-import {useEffect, useState} from "react";
+import {getSession, otpCheck} from "@/app/_actions/actions";
+import {useEffect, useRef, useState} from "react";
 import {notFound} from "next/navigation";
 import Link from "next/link";
+
+
+
 
 export default function Page() {
     const [session, setSession] = useState<string | undefined>(undefined);
     const [loading, setLoading] = useState(true);
+    const [otp, setOtp] = useState(new Array(6).fill(""));
+
+
+
 
     useEffect(() => {
         async function essa() {
@@ -18,21 +25,42 @@ export default function Page() {
         }
 
         essa();
+
     }, []);
 
     if (loading) return <div>Loading...</div>;
     if (!session) return notFound();
 
+    function handleChange(value: string, index: number) {
+        const newArr = [...otp];
+        newArr[index] = value;
+        setOtp(newArr);
+
+        if(value && index < 5){
+            const element = document.querySelector(`.input${index + 2}`) as HTMLElement | null;
+
+            if (element) {
+
+                console.log(otp)
+                element.focus();
+            } else {
+                console.warn(`Element with class .input${index} not found.`);
+            }
+        }
+    }
+
+
+
     return <div className="otpContainer" >
         <h1>Verify You Email Address</h1>
         <h2>To complete your registration, please verify your email address. Check your inbox for a verification link and follow the instructions to activate your account</h2>
-        <form action="" className="otpForm">
-            <input type="number" max={9} required={true}/>
-            <input type="number" max={9} required={true}/>
-            <input type="number" max={9} required={true}/>
-            <input type="number" max={9} required={true}/>
-            <input type="number" max={9} required={true}/>
+        <form action={otpCheck} className="otpForm">
 
+            <div className="inputsContainer">
+                {otp.map((digit, index) => (
+                    <input key={index} name={`value`} type="text" className={`input${index + 1}`} value={digit} onChange={e => handleChange(e.target.value, index)}/>
+                ))}
+            </div>
             <h3>Want to Change Your Email Address? <Link href="/" >Change here</Link></h3>
 
             <button className="submitForm">Verify Email</button>
