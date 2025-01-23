44f8de5a8bc7e5993faa98ae4de5ad27bd8f65b1
Termi <olafkrawczyk13@icloud.com>
2025-01-23 21:23:34 +0100

    Otp work

--- Next/opbio/app/_lib/sessions.ts
+++ Next/opbio/app/_lib/sessions.ts
@@ -31,9 +31,9 @@ export async function decrypt(session: string | undefined = '') {
     }
 }
 
-export async function createSession(userId: string) {
+export async function createSession(userId: string, otpVerified: boolean) {
     const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-    const session = await encrypt({ userId, expiresAt })
+    const session = await encrypt({ userId, expiresAt, otpVerified })
     const cookieStore = await cookies()
 
     cookieStore.set('session', session, {
