6a3301b2ead1829a88d36b7ab210d16b57b83ad6
Termi <olafkrawczyk13@icloud.com>
2025-02-01 16:47:35 +0100

    Upd

---
+++ Next/opbio/app/api/discord/route.ts
@@ -0,0 +1,19 @@
+
+
+export async function GET(request: Request) {
+
+    const url = new URL(request.url);
+
+
+    const code = url.searchParams.get('code');
+
+    if (!code) {
+        console.log('Brak parametru code w zapytaniu');
+        return new Response('Brak kodu autoryzacyjnego', { status: 400 });
+    }
+
+    console.log('Otrzymany kod:', code);
+
+
+    return new Response(`Otrzymano kod: ${code}`, { status: 200 });
+}
---
+++ Next/opbio/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import {Navbar} from "@/app/_components/navbar";
+
+export default function Page() {
+
+    return <Navbar />
+
+}
