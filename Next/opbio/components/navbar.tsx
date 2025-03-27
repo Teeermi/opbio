21a9ca218acf5cd129a73d8b25590f744611b08a
Termi <olafkrawczyk13@icloud.com>
2025-03-27 22:43:21 +0100

    Auth rework

---
+++ Next/opbio/components/navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import "./style.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <div className="navbarDashboard p-5">
+      <div className="flex flex-col justify-center items-center">
+        <img src="/image11.png" alt="" />
+
+        <Link
+          href="/dashboard"
+          className={`dashboardLink${
+            pathname === "/dashboard" ? "Active" : ""
+          }`}
+        >
+          <div
+            className={`sqrIcon${pathname === "/dashboard" ? "Active" : ""}`}
+          >
+            <i
+              className={`fa-solid fa-tablet awesome${
+                pathname === "/dashboard" ? "Active" : ""
+              }`}
+            ></i>
+          </div>
+          <h1 className={`h1Name${pathname === "/dashboard" ? "Active" : ""}`}>
+            Dashboard
+          </h1>
+        </Link>
+
+        <Link
+          href="/customize"
+          className={`dashboardLink${
+            pathname === "/customize" ? "Active" : ""
+          }`}
+        >
+          <div
+            className={`sqrIcon${pathname === "/customize" ? "Active" : ""}`}
+          >
+            <i
+              className={`fa-solid fa-tablet awesome${
+                pathname === "/customize" ? "Active" : ""
+              }`}
+            ></i>
+          </div>
+          <h1 className={`h1Name${pathname === "/customize" ? "Active" : ""}`}>
+            Customize
+          </h1>
+        </Link>
+      </div>
+      <div className="discordLinkContainer">
+        <div className="wrapperDiscordLinkContainer">
+          <div className="sqrDiscordIcon">
+            <i className="fa-brands fa-discord"></i>
+          </div>
+          <h1>Link discord?</h1>
+          <h2>Click button below</h2>
+        </div>
+
+        <Link href={"/"} className="discordLoginButton">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
